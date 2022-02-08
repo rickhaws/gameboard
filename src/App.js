0cd27316747e5f90ea2033c2abb1d5ac3c1e4c3b
Rick Haws <richard.haws@us.af.mil>
2022-02-07 17:21:29 -0700

    Added basic board with chess pieces

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
 import logo from './logo.svg';
 import './App.css';
+import * as Pieces from './Pieces'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="Board">
+		<div class="dark square">{Pieces.blackRook}</div>
+		<div class="light square">{Pieces.blackKnight}</div>
+		<div class="dark square">{Pieces.blackBishop}</div>
+		<div class="light square">{Pieces.blackQueen}</div>
+		<div class="dark square">{Pieces.blackKing}</div>
+		<div class="light square">{Pieces.blackBishop}</div>
+		<div class="dark square">{Pieces.blackKnight}</div>
+		<div class="light square">{Pieces.blackRook}</div>
+    <br/>
+
+		<div class="light square">{Pieces.blackPawn}</div>
+		<div class="dark square">{Pieces.blackPawn}</div>
+		<div class="light square">{Pieces.blackPawn}</div>
+		<div class="dark square">{Pieces.blackPawn}</div>
+		<div class="light square">{Pieces.blackPawn}</div>
+		<div class="dark square">{Pieces.blackPawn}</div>
+		<div class="light square">{Pieces.blackPawn}</div>
+		<div class="dark square">{Pieces.blackPawn}</div>
+    <br/>
+
+		<div class="dark square"></div>
+		<div class="light square"></div>
+		<div class="dark square"></div>
+		<div class="light square"></div>
+		<div class="dark square"></div>
+		<div class="light square"></div>
+		<div class="dark square"></div>
+		<div class="light square"></div>
+    <br/>
+
+		<div class="light square"></div>
+		<div class="dark square"></div>
+		<div class="light square"></div>
+		<div class="dark square"></div>
+		<div class="light square"></div>
+		<div class="dark square"></div>
+		<div class="light square"></div>
+		<div class="dark square"></div>
+    <br/>
+
+		<div class="dark square"></div>
+		<div class="light square"></div>
+		<div class="dark square"></div>
+		<div class="light square"></div>
+		<div class="dark square"></div>
+		<div class="light square"></div>
+		<div class="dark square"></div>
+		<div class="light square"></div>
+    <br/>
+
+		<div class="light square"></div>
+		<div class="dark square"></div>
+		<div class="light square"></div>
+		<div class="dark square"></div>
+		<div class="light square"></div>
+		<div class="dark square"></div>
+		<div class="light square"></div>
+		<div class="dark square"></div>
+    <br/>
+
+		<div class="dark square">{Pieces.whitePawn}</div>
+		<div class="light square">{Pieces.whitePawn}</div>
+		<div class="dark square">{Pieces.whitePawn}</div>
+		<div class="light square">{Pieces.whitePawn}</div>
+		<div class="dark square">{Pieces.whitePawn}</div>
+		<div class="light square">{Pieces.whitePawn}</div>
+		<div class="dark square">{Pieces.whitePawn}</div>
+		<div class="light square">{Pieces.whitePawn}</div>
+    <br/>
+
+		<div class="light square">{Pieces.whiteRook}</div>
+		<div class="dark square">{Pieces.whiteKnight}</div>
+		<div class="light square">{Pieces.whiteBishop}</div>
+		<div class="dark square">{Pieces.whiteQueen}</div>
+		<div class="light square">{Pieces.whiteKing}</div>
+		<div class="dark square">{Pieces.whiteBishop}</div>
+		<div class="light square">{Pieces.whiteKnight}</div>
+		<div class="dark square">{Pieces.whiteRook}</div>
+  </div>
   );
 }
 
 export default App;
+
---
+++ src/Pieces.js
@@ -0,0 +1,12 @@
+export const whiteKing =    String.fromCharCode(9812);  // ♔
+export const whiteQueen =	String.fromCharCode(9813);  // ♕
+export const whiteRook =	String.fromCharCode(9814);  // ♖
+export const whiteBishop =	String.fromCharCode(9815);  // ♗
+export const whiteKnight =	String.fromCharCode(9816);  // ♘
+export const whitePawn =	String.fromCharCode(9817);  // ♙
+export const blackKing =	String.fromCharCode(9818);  // ♚
+export const blackQueen =	String.fromCharCode(9819);  // ♛
+export const blackRook =	String.fromCharCode(9820);  // ♜
+export const blackBishop =	String.fromCharCode(9821);  // ♝
+export const blackKnight =	String.fromCharCode(9822);  // ♞
+export const blackPawn =	String.fromCharCode(9823);  // ♟
