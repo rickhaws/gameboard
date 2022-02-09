45fa2ff7ab77242143a3c4afa1d040baf6406645
Rick Haws <rickhaws@byu.net>
2022-02-09 09:53:16 -0700

    Experimenting with creating the board dynamically

--- src/App.js
+++ src/App.js
@@ -2,88 +2,75 @@ import logo from './logo.svg';
 import './App.css';
 import * as Pieces from './Pieces'
 
+function Square(color) {
+	return (
+		<div class={color + " square"}></div>
+	);
+}
+
+function Row(rowNumber) {
+	let rowType = rowNumber % 2;
+	let elements = [];
+	for (let i =0; i<8; i++) {
+		let color = i % 2 !== rowType ? "light" : "dark";
+		elements[i] = Square(color);
+	}
+	elements.push(<br/>);
+	return elements;
+}
+
+function Board() {
+	let rows = [];
+	for (let i=0; i<8; i++) {
+		rows.push(Row(i));
+	}
+	return rows;
+}
+
+class BitBoard {
+	size = 7;
+	constructor() {
+		this.state = [
+			[0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0],
+		];
+	}
+	set (row, column) {
+		this.state[row][column] = 1;
+	}
+	clear (row, column) {
+		this.state[row][column] = 0;
+	}
+	setAll() {
+		for (let i=0; i<this.size; i++) {
+			for (let j=0; j<this.size; j++) {
+				this.set(i, j);
+			}
+		}
+	}
+}
+
 function App() {
+	let activeSquares = new BitBoard();
+	for (let i = 0; i<3; i++) {
+		for (let j = 3-i; j < 7; i++) {
+			activeSquares.set(i,j);
+		}
+	}
+	for (let i = 4; i<7; i++) {
+		for (let j = 0; j < 7-i; j++) {
+			activeSquares.set(i,j);
+		}
+	}
+	
   return (
     <div id="Board">
-		<div class="dark square">{Pieces.blackRook}</div>
-		<div class="light square">{Pieces.blackKnight}</div>
-		<div class="dark square">{Pieces.blackBishop}</div>
-		<div class="light square">{Pieces.blackQueen}</div>
-		<div class="dark square">{Pieces.blackKing}</div>
-		<div class="light square">{Pieces.blackBishop}</div>
-		<div class="dark square">{Pieces.blackKnight}</div>
-		<div class="light square">{Pieces.blackRook}</div>
-    <br/>
-
-		<div class="light square">{Pieces.blackPawn}</div>
-		<div class="dark square">{Pieces.blackPawn}</div>
-		<div class="light square">{Pieces.blackPawn}</div>
-		<div class="dark square">{Pieces.blackPawn}</div>
-		<div class="light square">{Pieces.blackPawn}</div>
-		<div class="dark square">{Pieces.blackPawn}</div>
-		<div class="light square">{Pieces.blackPawn}</div>
-		<div class="dark square">{Pieces.blackPawn}</div>
-    <br/>
-
-		<div class="dark square"></div>
-		<div class="light square"></div>
-		<div class="dark square"></div>
-		<div class="light square"></div>
-		<div class="dark square"></div>
-		<div class="light square"></div>
-		<div class="dark square"></div>
-		<div class="light square"></div>
-    <br/>
-
-		<div class="light square"></div>
-		<div class="dark square"></div>
-		<div class="light square"></div>
-		<div class="dark square"></div>
-		<div class="light square"></div>
-		<div class="dark square"></div>
-		<div class="light square"></div>
-		<div class="dark square"></div>
-    <br/>
-
-		<div class="dark square"></div>
-		<div class="light square"></div>
-		<div class="dark square"></div>
-		<div class="light square"></div>
-		<div class="dark square"></div>
-		<div class="light square"></div>
-		<div class="dark square"></div>
-		<div class="light square"></div>
-    <br/>
-
-		<div class="light square"></div>
-		<div class="dark square"></div>
-		<div class="light square"></div>
-		<div class="dark square"></div>
-		<div class="light square"></div>
-		<div class="dark square"></div>
-		<div class="light square"></div>
-		<div class="dark square"></div>
-    <br/>
-
-		<div class="dark square">{Pieces.whitePawn}</div>
-		<div class="light square">{Pieces.whitePawn}</div>
-		<div class="dark square">{Pieces.whitePawn}</div>
-		<div class="light square">{Pieces.whitePawn}</div>
-		<div class="dark square">{Pieces.whitePawn}</div>
-		<div class="light square">{Pieces.whitePawn}</div>
-		<div class="dark square">{Pieces.whitePawn}</div>
-		<div class="light square">{Pieces.whitePawn}</div>
-    <br/>
-
-		<div class="light square">{Pieces.whiteRook}</div>
-		<div class="dark square">{Pieces.whiteKnight}</div>
-		<div class="light square">{Pieces.whiteBishop}</div>
-		<div class="dark square">{Pieces.whiteQueen}</div>
-		<div class="light square">{Pieces.whiteKing}</div>
-		<div class="dark square">{Pieces.whiteBishop}</div>
-		<div class="light square">{Pieces.whiteKnight}</div>
-		<div class="dark square">{Pieces.whiteRook}</div>
-  </div>
+		{Board()}
+  	</div>
   );
 }
 
